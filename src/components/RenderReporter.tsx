0a75695b9a1a5bf3a9547302218d0189e1827bee
rommguy <rommguy@gmail.com>
2022-05-10 13:44:18 +0300

    state and rendering

--- src/components/RenderReporter.tsx
+++ src/components/RenderReporter.tsx
@@ -1,4 +1,8 @@
+import IconButton from "@mui/material/IconButton";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import "./renderReporter.scss";
+import { useState } from "react";
 
 interface Props {
   indexInTree: number;
@@ -6,20 +10,38 @@ interface Props {
 }
 
 export const RenderReporter = ({ indexInTree, descendantsCount }: Props) => {
+  const [counter, setCounter] = useState<number>(0);
+
   console.log(`reporter-${indexInTree} rendered`);
+  const increment = () => {
+    setCounter((currentCounter) => currentCounter + 1);
+  };
+
+  const decrement = () => {
+    setCounter((currentCounter) => currentCounter - 1);
+  };
   return (
     <div className={`render-reporter reporter-${indexInTree}`}>
       {descendantsCount <= 0 ? (
         <div className="title">I'm the last one</div>
       ) : (
-        <>
-          <div className="title">I'm a render reporter</div>
-          <RenderReporter
-            indexInTree={indexInTree + 1}
-            descendantsCount={descendantsCount - 1}
-          />
-        </>
+        <div className="title">I'm a render reporter</div>
       )}
+      <div className="content">
+        <IconButton onClick={increment}>
+          <AddCircleOutlineIcon />
+        </IconButton>
+        <span className="counter-wrapper">{counter}</span>
+        <IconButton onClick={decrement}>
+          <RemoveCircleOutlineIcon />
+        </IconButton>
+      </div>
+      {descendantsCount > 0 ? (
+        <RenderReporter
+          indexInTree={indexInTree + 1}
+          descendantsCount={descendantsCount - 1}
+        />
+      ) : null}
     </div>
   );
 };
